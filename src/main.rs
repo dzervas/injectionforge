85c4aef52016f2a9dcf297ece82d10ead6d62b5f
Dimitris Zervas <dzervas@dzervas.gr>
2023-05-30 17:00:51 +0300

    Use upstream frida/frida-rust repo

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
 	let args: Vec<String> = std::env::args().collect();
 
 	if args.len() < 2 {
-		println!("Usage: {} <PID>", args[0]);
+		eprintln!("Usage: {} <PID>", args[0]);
 		return;
 	}
 
